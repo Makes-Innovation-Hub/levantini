7afb48e1f1bf161ea86192880fd898ad10cde260
Tante-Didi <didiarmoni@gmail.com>
2024-09-12 14:24:31 +0300

    saving changes in Button QuestionBox and TextQuestionType

---
+++ src/components/ui/QuestionBox/QuestionBox.style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components/macro";
+
+export const QuestionBox = styled.div`
+  display: flex;
+  flex-direction: column;
+ 
+`;
---
+++ src/pages/QuestionPage.jsx
@@ -0,0 +1,12 @@
+// import React from "react";
+import QuestionBox from "../components/ui/QuestionBox";
+
+const QuestionPage = () => {
+  return (
+    <main>
+      <QuestionBox />
+    </main>
+  );
+};
+
+export default QuestionPage;
