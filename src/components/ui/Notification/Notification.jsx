f7d251c971a5f3a72b7a61c7048bc9658c3a00b2
Rwaa Assi <rwaaassi123@gmail.com>
2024-09-15 10:38:56 +0300

    fixing toast component and notification

--- src/components/ui/Notification/Notification.jsx
+++ src/components/ui/Notification/Notification.jsx
@@ -1,40 +1,19 @@
-import { toast, Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Toast from "./Toast";
-import QuizNotification from "./QuizNotification";
-import QuizButton from "./QuizButton";
 
-const Notification = ({ isCorrect, isTimeout, explanation, handleNextQuestion }) => {
-  const title = isTimeout
-    ? "Time Out"
-    : isCorrect
-    ? "Yes! Right Answer"
-    : "OOPS! Wrong Answer";
-
-  const color = isTimeout || !isCorrect ? "var(--red--)" : "var(--green--)";
-
-  if (isCorrect || isTimeout) {
-    toast.custom(
-      (t) => (
-        <Toast color={color}>
-          <QuizNotification title={title} explanation={explanation} />
-          <QuizButton
-            handleClick={() => {
-              toast.dismiss(t.id);
-              handleNextQuestion();
-            }}
-          >
-            Next Question
-          </QuizButton>
-        </Toast>
-      ),
-      {
-        duration: isTimeout ? 10000 : Infinity,
-        position: "bottom-center",
-      },
-    );
-  }
-
-  return <Toaster />;
+const Notification = ({ title, color, explanation, handleNextQuestion }) => {
+  return (
+    <>
+      <Toaster />
+      <Toast
+        color={color}
+        title={title}
+        explanation={explanation}
+        handleNextQuestion={handleNextQuestion}
+        triggerToast={true}
+      />
+    </>
+  );
 };
 
 export default Notification;
--- src/components/ui/Notification/Toast.jsx
+++ src/components/ui/Notification/Toast.jsx
@@ -1,6 +1,30 @@
+import { toast } from "react-hot-toast";
+import QuizNotification from "./QuizNotification";
+import QuizButton from "./QuizButton";
 import * as S from "./Notification.style.js";
 
-const Toast = ({ color, children }) => (
-  <S.ToastContainer style={{ backgroundColor: color }}>{children}</S.ToastContainer>
-);
+const Toast = ({ color, title, explanation, handleNextQuestion, triggerToast }) => {
+  if (triggerToast) {
+    toast(
+      <S.ToastContainer style={{ backgroundColor: color }}>
+        <QuizNotification title={title} explanation={explanation} />
+        <QuizButton
+          handleClick={() => {
+            toast.dismiss();
+            handleNextQuestion();
+          }}
+        >
+          Next Question
+        </QuizButton>
+      </S.ToastContainer>,
+      {
+        duration: Infinity,
+        position: "bottom-center",
+      },
+    );
+  }
+
+  return null;
+};
+
 export default Toast;
