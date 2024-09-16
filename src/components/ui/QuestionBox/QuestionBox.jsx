64c424ece996c77ee8abe6b9746d71190e09f7a0
TanteDidi <danielarmoni@yahoo.com>
2024-09-16 10:40:15 +0300

    correcting colors variables in the QuestionBox component

--- src/components/ui/QuestionBox/QuestionBox.jsx
+++ src/components/ui/QuestionBox/QuestionBox.jsx
@@ -1,73 +1,23 @@
-// import React, { useState } from "react";
-//  import Button from "../Button/Button";
-//  import * as S from "./QuestionBox.style.js";
-// import dummyData from "../../../api/dummyData"; 
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import * as S from "./QuestionBox.style.js";
+import dummyData from "../../../api/dummyData";
 
-
-//  const QuestionBox = () => {
-//   const questionData = dummyData[0].questions[0];
-
-//   const [answerColors, setAnswerColors] = useState(Array(4).fill("var(--blue--)"));
-//   const [isAnswered, setIsAnswered] = useState(false);
-
-//   const handleAnswerClick = (index) => {
-//     if (isAnswered) return; // If already answered, do nothing
-
-//     const updatedColors = [...answerColors];
-
-//     if (index === questionData.correctAnswer) {
-//       updatedColors[index] = "var(--green--)";
-//     } else {
-//       updatedColors[index] = "var(--red--)";
-//       updatedColors[questionData.correctAnswer] = "var(--green--)";
-//     }
-
-//     setAnswerColors(updatedColors);
-//     setIsAnswered(true);
-//   };
-
-//   if (!questionData) return <p>No question data available</p>;
-
-//   return (
-//     <S.QuestionBox>
-        
-//       <p>{questionData.question}</p>
-//       {questionData.answers.map((answer, index) => (
-//         <Button
-//           key={index}
-//           handleClick={() => handleAnswerClick(index)}
-//           color={answerColors[index]}
-//         >
-//           {answer}
-//         </Button>
-//       ))}
-//     </S.QuestionBox>
-//   );
-// };
-
-// export default QuestionBox;
-
-
- import React, { useState } from "react";
-  import Button from "../Button/Button";
-  import * as S from "./QuestionBox.style.js";
-import dummyData from "../../../api/dummyData"; 
-
-import TextQuestion from "./../TextQuestionType/TextQuestionType.jsx";  
+import TextQuestion from "./../TextQuestionType/TextQuestionType.jsx";
 
 const QuestionBox = () => {
   const questionData = dummyData[0]?.questions[0];
   //  it is the same as dummyData[0]&& dummyData[0]?.questions[0];
-  const [answerColors, setAnswerColors] = useState(Array(4).fill("var(--blue--)"));
+  const [answerColors, setAnswerColors] = useState(Array(4).fill("var(--blue)"));
   const [isAnswered, setIsAnswered] = useState(false);
   const handleAnswerClick = (index) => {
     if (isAnswered) return; // If already answered, do nothing
     const updatedColors = [...answerColors];
     if (index === questionData.correctAnswer) {
-      updatedColors[index] = "var(--green--)";
+      updatedColors[index] = "var(--green)";
     } else {
-      updatedColors[index] = "var(--red--)";
-      updatedColors[questionData.correctAnswer] = "var(--green--)";
+      updatedColors[index] = "var(--red)";
+      updatedColors[questionData.correctAnswer] = "var(--green)";
     }
     setAnswerColors(updatedColors);
     setIsAnswered(true);
@@ -75,15 +25,11 @@ const QuestionBox = () => {
   if (!questionData) return <p>No question data available</p>;
   return (
     <>
-    
       {(() => {
         switch (questionData.questionType) {
           case "text":
             return (
-              <TextQuestion
-                question={questionData.question}
-                text={questionData.text}
-              />
+              <TextQuestion question={questionData.question} text={questionData.text} />
             );
           case "image":
             return (
@@ -104,7 +50,7 @@ const QuestionBox = () => {
             return <p>Unsupported question type</p>;
         }
       })()}
-     
+
       <S.QuestionBox>
         {questionData.answers.map((answer, index) => (
           <Button
@@ -119,13 +65,4 @@ const QuestionBox = () => {
     </>
   );
 };
-export default QuestionBox; 
-
-
-
-
-
-
-
-
-
+export default QuestionBox;
