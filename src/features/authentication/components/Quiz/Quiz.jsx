75c308f7bb109c778203f3b9fd9ec70b8e2d5aa3
TanteDidi <danielarmoni@yahoo.com>
2024-09-16 14:09:22 +0300

    adding the normalizer to the main

---
+++ src/features/authentication/components/Quiz/Quiz.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import * as S from "./TextQuestionType.styles";
+
+const Quiz = () => {
+  return <S.div>Quiz</S.div>;
+};
+
+export default Quiz;
---
+++ src/features/authentication/components/Quiz/Quiz.styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components/macro";
+import { breakpoints } from "../../../../styles/breakingpoints";
+
+export const div = styled.div`
+  @media (max-width: ${breakpoints.mobile}) {
+  }
+`;
