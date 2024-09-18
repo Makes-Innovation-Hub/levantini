70714c5ae67c387f3d1a250639ec095deb8c7db7
amjad-salameh <amjadsalameh00@gmail.com>
2024-09-18 12:37:35 +0300

    create navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Navbar = styled.div`
+  width: 96px;
+  height: 37px;
+  top: 16px;
+  left: 688px;
+  gap: 0px;
+  opacity: 0px;
+
+  font-family: var(--font-redressed);
+`;
+export default function Navbar() {
+  return <div>Levantini</div>;
+}
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import Navbar from "../components/Navbar/Navbar";
 
 const RootLayout = () => (
   <>
+  <Navbar/>
     <Toaster />
     <Outlet />
   </>
