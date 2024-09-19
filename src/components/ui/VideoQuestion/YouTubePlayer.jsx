d8c5c6e07842a2e50f00b31d53cb5a2acb15481f
Rwaa Assi <rwaaassi123@gmail.com>
2024-09-19 12:04:18 +0300

    used react-youtube lib

--- src/components/ui/VideoQuestion/YouTubePlayer.jsx
+++ src/components/ui/VideoQuestion/YouTubePlayer.jsx
@@ -1,63 +1,18 @@
-import { useRef, useEffect } from "react";
+import YouTube from "react-youtube";
 import * as S from "./VideoQuestion.styles.js";
 
 const YouTubePlayer = ({ videoId }) => {
-  const playerRef = useRef(null);
-
-  useEffect(() => {
-    const loadYouTubeAPI = () => {
-      const script = document.createElement("script");
-      script.src = "https://www.youtube.com/iframe_api";
-      script.async = true;
-      document.body.appendChild(script);
-
-      return () => {
-        document.body.removeChild(script);
-      };
-    };
-
-    const createPlayer = () => {
-      if (window.YT && window.YT.Player) {
-        if (playerRef.current) {
-          playerRef.current.destroy();
-        }
-        playerRef.current = new window.YT.Player(`youtube-player-${videoId}`, {
-          height: "188",
-          width: "330",
-          videoId: videoId,
-          playerVars: {
-            autoplay: 0,
-          },
-        });
-      } else {
-        console.error("YT.Player is not available");
-      }
-    };
-
-    if (!window.YT) {
-      loadYouTubeAPI();
-    }
-
-    const onYouTubeIframeAPIReady = () => {
-      createPlayer();
-    };
-
-    if (window.YT && window.YT.Player) {
-      createPlayer();
-    } else {
-      window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
-    }
-
-    return () => {
-      if (playerRef.current) {
-        playerRef.current.destroy();
-      }
-    };
-  }, [videoId]);
+  const opts = {
+    height: "188",
+    width: "330",
+    playerVars: {
+      autoplay: 0,
+    },
+  };
 
   return (
     <S.PlayerContainer>
-      <div id={`youtube-player-${videoId}`}></div>
+      <YouTube videoId={videoId} opts={opts} />
     </S.PlayerContainer>
   );
 };
