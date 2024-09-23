78ec7b41c23e966bb14be7bdd270937bb9a3afd1
amjad-salameh <amjadsalameh00@gmail.com>
2024-09-23 13:13:41 +0300

    create logo with counter

---
+++ src/components/ui/LogoHeader/LogoHeader.styles.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const LogoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CounterContainer = styled.div`
+  background-color: #47434d;
+  border-radius: 12px;
+  padding: 5px 10px;
+  color: var(--white);
+  font-size: 16px;
+  margin-top: 10px;
+`;
+
+export const LogoSvgContainer = styled.div`
+  position: relative;
+  width: 48px;
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const LogoText = styled.div`
+  position: absolute;
+  font-family: var(--font-rubik);
+  font-size: 20px;
+  font-style: italic;
+  font-weight: 400;
+  line-height: 13.3px;
+  color: var(--black);
+  font-family: Arial, sans-serif;
+`;
--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Header from "../components/Header/Header";
+import LogoHeader from "../components/ui/LogoHeader/LogoHeader";
 
 const RootLayout = () => (
   <>
+    <LogoHeader />
     <Header />
     <Toaster />
     <Outlet />
