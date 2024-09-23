03367a3a3173fdf58798329f697454d2a740c500
TanteDidi <danielarmoni@yahoo.com>
2024-09-23 12:27:09 +0300

    adding use-effect to the toast

--- src/components/ui/Notification/Toast.jsx
+++ src/components/ui/Notification/Toast.jsx
@@ -1,27 +1,30 @@
 import { toast } from "react-hot-toast";
+import { useEffect } from "react";
 
 const Toast = ({ open, children, duration, position, width, toastId = "quiz" }) => {
-  if (open) {
-    toast.custom(
-      (t) => (
-        <div
-          className={`toast `}
-          style={{
-            width: width,
-          }}
-        >
-          {children}
-        </div>
-      ),
-      {
-        duration: duration,
-        position: position,
-        id: toastId,
-      },
-    );
-  } else {
-    toast.dismiss();
-  }
+  useEffect(() => {
+    if (open) {
+      toast.custom(
+        (t) => (
+          <div
+            className={`toast `}
+            style={{
+              width: width,
+            }}
+          >
+            {children}
+          </div>
+        ),
+        {
+          duration: duration,
+          position: position,
+          id: toastId,
+        },
+      );
+    } else {
+      toast.dismiss(toastId);
+    }
+  }, [open, children, duration, position, width, toastId]);
 
   return null;
 };
