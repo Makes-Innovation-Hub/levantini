3ebfbdfadf9cec7baed0c130ec5bb694e6d32064
amjad-salameh <amjadsalameh00@gmail.com>
2024-09-29 19:53:20 +0300

    create category status ui

---
+++ src/components/ui/CategoryStatus/CategoryStatus.styles.js
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+export const Button = styled.button`
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  padding: 10px 20px;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  background-color: ${(props) =>
+    props.status === "completed"
+      ? "#58D39F"
+      : props.status === "started"
+        ? "#58B2D3"
+        : props.status === "in-progress"
+          ? "#D35858"
+          : "#D35858"};
+  color: white;
+  transition: background-color 0.3s ease;
+  &:disabled {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+  svg {
+    fill: white;
+  }
+`;
+export const LockMessage = styled.div`
+margin-top: 20px;
+  padding: 20px;
+  border: 2px dashed #FF4081;
+  color: #FF4081;
+  font-family: var(--inter)
+  font-size: 10px;
+  font-weight: 700;
+  border-radius: 10px;
+  text-align: center;
+  max-width: 300px;
+  }
+`;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -17,6 +17,7 @@ const Home = () => {
 
   return (
     <>
+     
       <S.CategoryContainer>
         <S.Title>Choose your category of game</S.Title>
         <S.GridContainer>
