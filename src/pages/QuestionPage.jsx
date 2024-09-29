c8ccebf9e7b710a97e229ebcfe482ef0132d7654
TanteDidi <danielarmoni@yahoo.com>
2024-09-29 11:40:11 +0300

    next Question works

--- src/pages/QuestionPage.jsx
+++ src/pages/QuestionPage.jsx
@@ -37,7 +37,7 @@ const QuestionPage = () => {
           title={notification.title}
           color={notification.color}
           explanation={notification.explanation}
-          // handleNextQuestion={handleNextQuestion}
+          handleNextQuestion={handleNextQuestion}
         >
           {" "}
         </Notification>
