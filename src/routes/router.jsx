f69e620b2b3f2463cd944ba1ab441a99f657f959
Rulaazz <rula.abozaid@gmail.com>
2024-10-10 13:15:53 +0300

    Meta+Menu+framer-motion

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,10 +1,10 @@
 import * as React from "react";
 import { createBrowserRouter } from "react-router-dom";
-import { LOGIN, QUESTION_PAGE, ROOT } from "./routeConstants";
+import { LOGIN, QUESTION_PAGE, ROOT ,MENU} from "./routeConstants";
 import Home from "@/pages/Home/Home";
 import QuestionPage from "../pages/QuestionPage";
+import Menu from "../components/menu/Menu";
 import RootLayout from "../layouts/RootLayout";
-import Page404 from "@/pages/Page404/Page404";
 const Login = React.lazy(() => import("@pages/Login/Login"));
 
 const router = createBrowserRouter([
@@ -16,15 +16,18 @@ const router = createBrowserRouter([
 
       {
         path: QUESTION_PAGE,
-        element: <QuestionPage />,
+        element: <QuestionPage />, //{"questionCategory/:categoryId/", element: <QuestionPage />},
       },
       {
         path: LOGIN,
         element: <Login />,
       },
+         {
+        path: MENU,
+        element: <Menu />,
+      },
+      { path: "*", element: <div>Not found</div> },
     ],
   },
-  { path: "*", element: <Page404 /> },
 ]);
-
 export default router;
