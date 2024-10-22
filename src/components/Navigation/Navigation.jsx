d484c023444d37544a9f7cb1bdfe473e35dd8de9
Roninhij <ronin1hij@hotmail.com>
2024-10-22 16:55:17 +0300

    finished fixing the ham

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { MdHome } from "react-icons/md";
 import { LiaCrownSolid } from "react-icons/lia";
-import { IoIosLogOut } from "react-icons/io";
 import { LogoutGoogle } from "../../features/authentication";
 import { useAuth } from "../../features/authentication/context/AuthContext";
 import { Link } from "react-router-dom";
+import "./Navigation.css";
 
 const Navigation = ({ closeMenu }) => {
   const { logout } = useAuth();
@@ -12,13 +12,15 @@ const Navigation = ({ closeMenu }) => {
   return (
     <ul className="menu-options">
       <li onClick={closeMenu}>
+        <MdHome className="theIcons" />
         <Link to="/" className="firstLink">
-          <MdHome className="theIcons" /> Home
+          <span className="HomeIcon">Home</span>
         </Link>
       </li>
       <li onClick={closeMenu}>
+        <LiaCrownSolid className="theIcons" />
         <Link to="/LeaderBoard" className="firstLink">
-          <LiaCrownSolid className="theIcons" /> Leaderboard
+          <span className="HomeIcon">Leaderboard</span>
         </Link>
       </li>
       <li className="secondLink">
